6b388a9ecd837a2c3234df8f8eaba2fbc1c441d4
rokan45 <rokanuzzaman4567@gmail.com>
2026-02-20 01:59:34 +0600

    connect homepage with successful login using js

--- script/login.js
+++ script/login.js
@@ -1,1 +1,24 @@
 console.log("Im connected")
+
+document.getElementById('login-btn').addEventListener('click',function(){
+    //get the number
+    const contactNumber=document.getElementById('input-number');
+    const getNumber=contactNumber.value;
+    
+    //get the pin
+    const userPin=document.getElementById('input-pin');
+    const getPin=userPin.value;
+   
+    //match the pin and number
+    if(getNumber=='01711111111' && getPin=="1122"){
+         //--if match go to homepage
+        alert("Go to homepage");
+        window.location.replace("homepage.html");
+    }
+    else{
+         //--if not reject
+        alert("Input valid number and password");
+    }
+   
+   
+})
